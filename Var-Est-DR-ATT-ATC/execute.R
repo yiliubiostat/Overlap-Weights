9c6ad0900bc3eb6309fa5b815cb33f113028f7f1
Yi Liu <90741171+yiliu72@users.noreply.github.com>
2022-01-27 02:52:29 -0500

    Add files via upload

---
+++ Var-Est-DR-ATT-ATC/execute.R
@@ -0,0 +1,11 @@
+source("md1_wboot.R")
+source("md2_wboot.R")
+source("md3_wboot.R")
+
+source("md1_sim.R")
+source("md2_sim.R")
+source("md3_sim.R")
+
+source("md1_wboot_cons.R")
+source("md2_wboot_cons.R")
+source("md3_wboot_cons.R")
